78eb798d90a27eeccf9ea9a141617c417a417daa
maxwell150 <maxwelgithumbi@gmail.com>
2021-11-14 12:05:58 +0000

    test

---
+++ tests/test_models/test_Base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+"""
+test
+"""
+
+import unittest
+import os
+from models.base_model import BaseModel
+import json
+
+class TestBaseModel(unittest.TestCase):
+    """
+    testing the base model class
+    """
+
+    def setUp(self):
+        self.model = BaseModel()
+        return super().setUp()
+
+    def tearDown(self):
+        del(self.model)
+        if os.path.exists('file.json'):
+            os.remove('file.json')
+        return super().tearDown()
+
+    def test_instatiation(self):
+        """
+        checking whether self.model is an instance of BaseModel
+        """
+        self.assertIsInstance(self.model, BaseModel)
+
+
+    def test_Dict_instance(self):
+        """
+        checking if instace give the correct output
+        """
+        dict_model = {
+                      'my_number' : 89,
+                      'name' : 'Holberton',
+                      '__class__' : 'BaseModel',
+                      'updated_at' : '2017-09-28T21:05:54.119572',
+                      'id' : 'b6a6e15c-c67d-4312-9a75-9d084935e579',
+                      'created_at' : '2017-09-28T21:05:54.119427'
+                     }
+        myModel = BaseModel(**model_dict)
+        self.assertIsInstance(myModel, BaseModel)
+        self.assertEqual(myModel.my_number, 89)
+        self.assertEqual(myModel.name, 'Holberton')
+        self.assertEqual(myModel.id, 'b6a6e15c-c67d-4312-9a75-9d084935e579')
+
+if __name__ == '__main__':
+    unittest.main()
