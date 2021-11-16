29820d8c85fc5a17c16666ac921c90dae2f23166
fanuelal <fanuelalmawstop.com>
2021-11-16 04:42:51 -0800

    models change

--- 0test.py
+++ 0test.py
@@ -5,14 +5,10 @@ my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
 print(my_model)
-print('\n')
 my_model.save()
 print(my_model)
-print('\n')
 my_model_json = my_model.to_dict()
 print(my_model_json)
-print('\n')
 print("JSON of my_model:")
-print('\n')
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
