b84075d548f7d6372e5e95122e6553bf6afeccd2
fanuelal <fanuelalmawstop.com>
2021-11-15 21:55:56 -0800

    base model

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+"""
+ To create a unique FileStorage instance for your application
+"""
+
+from .engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+#storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ attributes/methods for other classes
 
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """
@@ -22,16 +22,16 @@ class BaseModel:
 
         dateFormat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) is not None:
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, dateFormat)
                 else:
                     self.__dict__[key] = value
         else:
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """
@@ -46,12 +46,12 @@ class BaseModel:
         attribute updated_at with the current datetime
         """
 
-        self.updated_at = datetime.today()
-        models.storage.save()
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
-        return dict rep of the obj
+        return dict representation of the obj
         """
         dict_repr = {}
         for key, value in self.__dict__.items():
